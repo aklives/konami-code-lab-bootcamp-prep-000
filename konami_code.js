5cbaea777715dbfa0d087ee65fdceddfd1365884
Adam Kearney <aklives@hotmail.com>
2019-03-05 21:05:24 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -15,7 +15,7 @@ let index = 0;
 
 function init() {
   var body = document.querySelector('body');
-  body.addEventListener('keydown', function(event) {
+  body.addEventListener('keydown', function(e) {
   function onKeyDownHandler(e) {
   const key = e.key;
   while (codes[index] < codes.length){
