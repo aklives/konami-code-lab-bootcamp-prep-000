242ad1c30b24112a1821e3e08bd6c50a96e44fb3
Adam Kearney <aklives@hotmail.com>
2019-03-05 21:47:48 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -11,30 +11,24 @@ const codes = [
   "a"
 ];
 
-let index = 0;
+var index = 0;
 
 function init() {
-  var body = document.querySelector('body');
-  body.addEventListener('keydown', function(e) {
-  var key = e.key;
-  
-  if (key === codes[index]) {
-    console.log(key);
-    console.log(index);
-    index++;
-    console.log(index);
-    
-    
-   if (index == codes.length){
-     alert("Hurray!");
-     index = 0;
-   }
-    
-  }
-   else {
-    index = 0;
-   }
-});
-var index = 0;
+  let idx = 0;
+
+  document.body.addEventListener("keydown", (e) => {
+    const key = e.key;
+
+    idx = (codes[idx] === key) ? ++idx : 0
+
+
+    if (idx === codes.length) {
+      window.alert("Hurray!");
+      idx = 0
+    }
+
+  });
+}
+index = 0;
 }
 
